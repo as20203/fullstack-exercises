3ec032fcdd5cd65af03a563637dae1cbf60848ea
Jawad Zaheer <jzaheer.bese16seecs@seecs.edu.pk>
2022-12-09 19:47:28 +0200

    Add part 4 exercises 4.1 and 4.2

---
+++ part4/bloglist/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+app.use(middleware.requestLogger)
+
+
+const blogsRouter = require('./controller/blogs')
+app.use('/api/blogs', blogsRouter)
+
+const PORT = 3003
+app.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`)
+})
